8a30bbdd563434efaaebf5effd172237ef9dbeeb
Tim <exelent206@gmail.com>
2022-10-28 16:39:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-//document.location.reload()
 const c = console.log
-
-window.addEventListener('load', function(){
     let xhr = new XMLHttpRequest()
     xhr.open("GET", "https://nbvtim.github.io/work/SOURS/db.json", false) // данные берутся с сервера, возможна задержка 
     xhr.send()
+window.addEventListener('load', function(){
 
     let div = document.createElement("div")
     input_.addEventListener('input', function(){
